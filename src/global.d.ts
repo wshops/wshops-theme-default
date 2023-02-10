f45cffbdfb14bcc00947a13b5cffd0127b9adcab
Tony An <anxuanzi@sh-c.cn>
2023-02-10 10:29:30 -0600

    :sparkles: add captcha

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,7 @@
 import { Alpine as AlpineType } from 'alpinejs'
 import { Notify } from './utils/notify'
 import WshopUtils from '@wshops/utils'
+import '@hcaptcha/types'
 
 declare global {
   interface Window {
--- src/pages/login/main.ts
+++ src/pages/login/main.ts
@@ -1,13 +1,13 @@
-import "./style.less";
-import Alpine from "alpinejs";
-import WshopUtils, { FormValidationResult } from "@wshops/utils";
-import { useNotify } from "../../utils/notify";
+import './style.less'
+import Alpine from 'alpinejs'
+import WshopUtils, { FormValidationResult } from '@wshops/utils'
+import { useNotify } from '../../utils/notify'
 
 useNotify({
-  position: "top-right",
-});
+  position: 'top-right',
+})
 /****** 初始化 ******/
-window.Alpine = Alpine;
+window.Alpine = Alpine
 const wshop: WshopUtils = new WshopUtils({
   feedbacks: {
     formValidationFeedbacks: {
@@ -17,12 +17,12 @@ const wshop: WshopUtils = new WshopUtils({
           (result.inputElement as HTMLInputElement).labels!.length > 0
         ) {
           (result.inputElement as HTMLInputElement).labels![0].className =
-            "block mb-2 text-sm font-medium text-green-700 dark:text-green-500";
+            'block mb-2 text-sm font-medium text-green-700 dark:text-green-500'
         }
         result.inputElement.className =
-          "bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-green-500";
-        if (document.getElementById(result.inputElement.id + "-error") !== null)
-          document.getElementById(result.inputElement.id + "-error")!.remove();
+          'bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-green-500'
+        if (document.getElementById(result.inputElement.id + '-error') !== null)
+          document.getElementById(result.inputElement.id + '-error')!.remove()
       },
       onInvalid: (result: FormValidationResult): void => {
         if (
@@ -30,105 +30,134 @@ const wshop: WshopUtils = new WshopUtils({
           (result.inputElement as HTMLInputElement).labels!.length > 0
         ) {
           (result.inputElement as HTMLInputElement).labels![0].className =
-            "block mb-2 text-sm font-medium text-red-700 dark:text-red-500";
+            'block mb-2 text-sm font-medium text-red-700 dark:text-red-500'
         }
         result.inputElement.className =
-          "bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 block w-full p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500";
+          'bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 block w-full p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500'
         if (result.message !== null) {
           if (
-            document.getElementById(result.inputElement.id + "-error") ===
-              null ||
-            document.getElementById(result.inputElement.id + "-error") ===
-              undefined
+            document.getElementById(result.inputElement.id + '-error') ===
+            null ||
+            document.getElementById(result.inputElement.id + '-error') ===
+            undefined
           ) {
             result.inputElement.insertAdjacentHTML(
-              "afterend",
+              'afterend',
               `<p class="mt-2 text-sm text-red-600 dark:text-red-500" id="${result.inputElement.id}-error">${result.message}</p>`
-            );
+            )
           } else {
             document.getElementById(
-              result.inputElement.id + "-error"
-            )!.innerHTML = <string>result.message;
+              result.inputElement.id + '-error'
+            )!.innerHTML = <string>result.message
           }
         }
       },
     },
     apiFeedbacks: {
       onError: (message: string): void => {
-        window.$notify.closable().error(message);
+        window.$notify.closable().error(message)
       },
       onInfo: (message: string): void => {
-        window.$notify.closable().info(message);
+        window.$notify.closable().info(message)
       },
       onWarning: (message: string): void => {
-        window.$notify.closable().warn(message);
+        window.$notify.closable().warn(message)
       },
       onUnAuthorized: (): void => {
-        window.location.assign("/auth/register");
+        window.location.assign('/auth/register')
       },
       onSuccess: (message: string): void => {
-        window.$notify.closable().success(message);
+        window.$notify.closable().success(message)
       },
     },
   },
-});
+})
 
 // n.closable().info('hello world')
 
 /************ UI交互及动效逻辑 ************/
 //页面交互业务逻辑？？？(alpine 用起来跟 VUE 差不多？)
 
-Alpine.start();
+Alpine.start()
 /***************** 结束 *****************/
+
+/***************** hcaptcha *****************/
+// declare global {
+//   interface Window {
+//     hcaptchaCustomCallbackFunc: (token: string) => void
+//   }
+// }
+
+// window.hcaptchaCustomCallbackFunc = (token) => {
+//   if (document.getElementById('captcha_token_input') === undefined || document.getElementById('captcha_token_input') === null) {
+//     const inputElement = document.createElement('input')
+//     inputElement.setAttribute('type', 'hidden')
+//     inputElement.setAttribute('id', 'captcha_token_input')
+//     inputElement.setAttribute('name', 'captcha_token')
+//     inputElement.setAttribute('value', token)
+//     document.getElementById('hcaptcha-block')!.insertAdjacentElement('afterend', inputElement)
+//   } else {
+//     document.getElementById('captcha_token_input')!.removeAttribute('value')
+//     document.getElementById('captcha_token_input')!.setAttribute('value', token)
+//   }
+// }
+/***************** hcaptcha end *****************/
 
 /************ 表单验证配置 ************/
 // 初始化验证器实例并定义表单验证规则（如果开启 async 模式则声明完规则自动开始校验每一次的输入）
 let c = wshop.vd(true).init([
   {
-    element: document.getElementById("username")!,
+    element: document.getElementById('username')!,
     rules: [
       {
-        validatorName: "required",
-        invalidMessage: "用户名不能为空",
+        validatorName: 'required',
+        invalidMessage: '用户名不能为空',
       },
     ],
   },
   {
-    element: document.getElementById("password")!,
+    element: document.getElementById('password')!,
     rules: [
       {
-        validatorName: "required",
-        invalidMessage: "密码不能为空",
+        validatorName: 'required',
+        invalidMessage: '密码不能为空',
       },
       {
         customValidator: (value: string): boolean => {
-          return value.length >= 6;
+          return value.length >= 6
         },
-        invalidMessage: "密码长度不能小于6位",
+        invalidMessage: '密码长度不能小于6位',
       },
     ],
   },
-]);
+])
 /*************** 结束 ****************/
 
-document.getElementById("login-form")!.addEventListener("submit", (e) => {
-  e.preventDefault();
+document.getElementById('login-form')!.addEventListener('submit', (e) => {
+  e.preventDefault()
   if (!c.validate().getResult()) {
-    return;
+    return
   }
-  const formData = wshop.formDataToObject("login-form");
-  formData["password"] = wshop.md5(formData["password"] as string);
+  const formData = wshop.formDataToObject('login-form')
+  if (formData['h-captcha-response'] !== undefined && formData['h-captcha-response'] !== null && formData['h-captcha-response'] !== '') {
+    formData['captcha_token'] = formData['h-captcha-response']
+    delete formData['h-captcha-response']
+    delete formData['g-recaptcha-response']
+  }
+  formData['password'] = wshop.md5(formData['password'] as string)
   wshop
     .api()
-    .post("/api/v1/capi/auth/login", formData)
+    .post('/api/v1/capi/auth/login', formData)
     .then((res) => {
       if (res !== null && res !== undefined) {
-        console.log(res);
+        console.log(res)
         //has valid response
-        location.assign("index");
+        location.assign('index')
       }
     })
     .catch((err) => {
-      window.$notify.error(err);
-    });
-});
+      window.$notify.error(err).then(() => {
+        hcaptcha.reset('hcaptcha-block')
+      })
+    })
+})
