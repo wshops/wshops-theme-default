b0a990f1ee1da84ff3be3117008bf09f930410f6
wth <1287691424@qq.com>
2023-04-19 17:54:24 +0800

    去掉alpinejs

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,14 +1,12 @@
-import { Alpine as AlpineType } from "alpinejs";
+// import { Alpine as AlpineType } from "alpinejs";
 import { Notify } from "./utils/notify";
 import WshopUtils from "@wshops/utils";
 import "@hcaptcha/types";
 
 declare global {
   interface Window {
-    Alpine: AlpineType;
-
+    // Alpine: AlpineType;
     $notify: Notify;
-
     $wshop: WshopUtils;
     refreshAddressList: Function;
     addressId: "";
